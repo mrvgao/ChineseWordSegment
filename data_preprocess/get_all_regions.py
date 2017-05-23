0945137c3f51ce8ba5feffa0e4e3bd0e04a0d549
fortymiles <mqgao@outlook.com>
2017-05-23 12:52:06 +0800

    get all regions

--- data_preprocess/get_all_regions.py
+++ data_preprocess/get_all_regions.py
@@ -10,17 +10,21 @@ content_file = 'train_content.txt'
 
 total_cut_words = []
 
+mini_test_batch = None
+
 with open(content_file) as f:
     for index, line in enumerate(f.readlines()):
         total_cut_words.append(list(jieba.cut(line)))
         if index % 100 == 0:
             print(index)
 
+        if mini_test_batch is not None and index > mini_test_batch:
+            break
+
 all_regions = 'all_regions'
 
 with open(all_regions, 'wb') as f:
-    pickle.dump(all_regions, f, pickle.HIGHEST_PROTOCOL)
+    pickle.dump(total_cut_words, f, pickle.HIGHEST_PROTOCOL)
 
 logging.info('pickle done!')
-
 
